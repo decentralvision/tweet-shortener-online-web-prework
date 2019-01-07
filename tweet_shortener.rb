8dbc85b5ddc9ddb3fea76353ed5a69aba6c32dc2
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-07 21:10:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,13 @@
 # Write your code here.
+dict = {
+  'hi' => ['hello'],
+  '2' => ['to', 'two', 'too'],
+  '4' => ['for', 'four'],
+  'b' => ['be'],
+  'you'
+}
 def dictionary(words)
   words.collect do |word|
-
+    
 def word_substituter(tweet)
   dictionary(tweet.split(' ')).join(' ')
