5172adda1a3cb552d7f591b54fa51294a4286bf3
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-07 21:19:21 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,15 +1,18 @@
 # Write your code here.
-dict = {
-  'hi' => ['hello'],
-  '2' => ['to', 'two', 'too'],
-  '4' => ['for', 'four'],
-  'b' => ['be'],
-  'u' => ['you'],
-  '@' => ['at'],
-  '&' => ['and']
-}
+
 
 def dictionary(words)
+  
+  dict = {
+    'hi' => ['hello'],
+    '2' => ['to', 'two', 'too'],
+    '4' => ['for', 'four'],
+    'b' => ['be'],
+    'u' => ['you'],
+    '@' => ['at'],
+    '&' => ['and']
+  }
+
   words.collect do |word|
     dict.each do |short_word, long_word|
       value.each do |long_word|
