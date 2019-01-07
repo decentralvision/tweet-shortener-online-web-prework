3bbfe80e80871bc259f19aefeb2c154e2ed6cf78
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-07 21:46:01 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -37,7 +37,7 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.size > 140
+  if tweet.size >= 140
     word_substituter(tweet)
   elsif tweet.size < 130
     tweet
