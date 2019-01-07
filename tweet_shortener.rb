6027446d46893033aa6b4f032fd4be9c7116f768
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-07 21:19:35 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,7 +15,7 @@ def dictionary(words)
 
   words.collect do |word|
     dict.each do |short_word, long_word|
-      value.each do |long_word|
+      long_word.each do |long_word|
         if word == long_word
           word = long_word.key
         end
